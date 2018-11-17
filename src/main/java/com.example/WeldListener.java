6f75d20574b8ed4025371db6a5be9ea4ba883d34
elad.chen <eladchen@gmail.com>
2018-11-17 13:21:06 +0200

    latest

--- src/main/java/com.example/WeldListener.java
+++ src/main/java/com.example/WeldListener.java
@@ -1,15 +1,14 @@
 package com.example;
 
 import org.jboss.resteasy.cdi.ResteasyCdiExtension;
-import org.jboss.weld.bootstrap.BeanDeploymentModules;
 import org.jboss.weld.bootstrap.spi.BeanDiscoveryMode;
 import org.jboss.weld.bootstrap.spi.EEModuleDescriptor;
 import org.jboss.weld.bootstrap.spi.helpers.EEModuleDescriptorImpl;
 import org.jboss.weld.config.ConfigurationKey;
-import org.jboss.weld.configuration.spi.helpers.ExternalConfigurationBuilder;
-import org.jboss.weld.environment.ContainerInstanceFactory;
 import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.servlet.Listener;
 import org.jboss.weld.environment.servlet.services.ServletResourceInjectionServices;
+
 import org.reflections.Reflections;
 import org.reflections.scanners.ResourcesScanner;
 import org.reflections.scanners.SubTypesScanner;
@@ -23,13 +22,6 @@ import javax.servlet.ServletContextListener;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-
-/**
- * @see org.jboss.weld.config.ConfigurationKey
- */
-
-import static org.jboss.weld.config.ConfigurationKey.BEAN_IDENTIFIER_INDEX_OPTIMIZATION;
-import static org.jboss.weld.environment.servlet.Listener.CONTAINER_ATTRIBUTE_NAME;
 
 public class WeldListener implements ServletContextListener {
     private final Weld weld = new Weld();
@@ -51,8 +43,8 @@ public class WeldListener implements ServletContextListener {
     public void contextInitialized( ServletContextEvent sce ) {
         // weld.containerId( "my-custom-cdi" ); // This is not unique between runs (& not shutdown properly).
 
-        // No sure what happens when "disableDiscovery" is on,
-        // And "scanClasspathEntries" is on.
+        // No sure what happens when both
+        // "disableDiscovery" & "scanClasspathEntries" are on
         weld.scanClasspathEntries();
         weld.disableDiscovery();
 
@@ -70,13 +62,14 @@ public class WeldListener implements ServletContextListener {
         weld.addServices( new ServletResourceInjectionServices() {} );
         weld.addServices( new EEModuleDescriptorImpl( sce.getServletContext().getContextPath(), EEModuleDescriptor.ModuleType.WEB ) );
 
-        weld.addProperty( BEAN_IDENTIFIER_INDEX_OPTIMIZATION.get(), Boolean.FALSE.toString() );
+        weld.addProperty( ConfigurationKey.BEAN_IDENTIFIER_INDEX_OPTIMIZATION.get(), Boolean.FALSE.toString() );
         weld.addProperty( ConfigurationKey.CONCURRENT_DEPLOYMENT.get(), Boolean.FALSE.toString() );
+        weld.addProperty( Weld.ARCHIVE_ISOLATION_SYSTEM_PROPERTY, true );
 
         // This shouldn't here.
         // This is obviously only relevant when RestEasy is in play.
         weld.addExtension( new ResteasyCdiExtension() );
 
-        sce.getServletContext().setAttribute( CONTAINER_ATTRIBUTE_NAME, weld );
+        sce.getServletContext().setAttribute( Listener.CONTAINER_ATTRIBUTE_NAME, weld );
     }
 }
